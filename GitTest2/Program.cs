8249a3ca045e8f7ce44854aa6214afd9f48eb0a5
jamesmit <jamesmit@CSG1-522512>
2017-01-24 16:10:45 -0600

    updateed to a guessing game

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -10,7 +10,39 @@ namespace GitTest2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randNum = new Random();
+            int numbGuesses = 0;
+            int number = randNum.Next(1, 101);
+            Console.WriteLine("Choose a number from 1-100:");
+            int numbChosen = Convert.ToInt32(Console.ReadLine());
+            while(numbChosen != number)
+            {
+                if(numbChosen > number)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                numbGuesses++;
+                Console.WriteLine("Please choose again:");
+                numbChosen = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Congratulations you guessed the number");
+            Console.WriteLine("You guessed " + numbGuesses + " times.");
+            if(numbGuesses < 10)
+            {
+                Console.WriteLine("Wow that didn't take you long");
+            }
+            else if(numbGuesses > 10 && numbGuesses < 30)
+            {
+                Console.WriteLine("That took you about average amount of tries");
+            }
+            else
+            {
+                Console.WriteLine("That too you forever to guess");
+            }
             Console.ReadKey();
         }
     }
